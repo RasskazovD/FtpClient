a2b3d481b6d3d1c20a647f4d1e84d6fa47a1c4f9
rasskazovdv <rasskazovdv@HOME>
2019-12-10 19:30:04 +0200

    Начало

---
+++ FtpClient2/ConfigLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FtpClient2
+{
+    class ConfigLoader
+    {
+        public string Main()
+        {
+            String line;
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\Config.txt"))
+                {
+                    line = sr.ReadToEnd();
+                }
+            }
+            catch(IOException e)
+            {
+                line = "No config file";
+            }
+            return line;
+        }
+    }
+}
